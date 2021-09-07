0d8ad6e5950c1804e5a05f52b33da44e57ac007d
Tchilo <tchiloross@gmail.com>
2021-09-07 18:18:35 +0200

    Add About page, load featured section dynamically, link home page and about page

---
+++ about.js
@@ -0,0 +1,13 @@
+function grab(e) {
+  return document.querySelector('.' + e);
+}
+
+// ABOUT PAGE MENU
+
+grab('fa-bars').addEventListener('click', () => {
+  grab('menu-lis', false, false).style.display = 'flex';
+});
+
+grab('close').addEventListener('click', () => {
+  grab('menu-lis', false, false).style.display = 'none';
+});
